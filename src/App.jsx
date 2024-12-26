180cb8f786dfd1ec13fa1ce9681957056b2af138
avinash23em <ap23vi@gmail.com>
2024-12-26 22:08:13 +0530

    intial

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,62 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {RecoilRoot, atom , useRecoilValue, useSetRecoilState} from 'recoil'
+import { counteratom } from './Store/atoms/Counter'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <p>what i am doing even </p>
+      <RecoilRoot>
+           <Counter/>
+      </RecoilRoot>
     </>
   )
 }
+function Counter(){
+return (
+  <div>
+    <Currcounter/>
+    <Increase/>
+    <Decrease/>
+  </div>
+)
+}
+function Currcounter(){
+const count=useRecoilValue(counteratom);
+return (
+  <div>
+    count {count}
+  </div>
+)
+}
+
+function Increase(){
+  const setCount=useSetRecoilState(counteratom);
+  function increase(){
+setCount(c=>c+1);
+  }
+  return (
+    <div>
+      <button onClick={increase}>increase</button>
+    </div>
+  )
+}
+
+function Decrease(){
+  const setCount=useSetRecoilState(counteratom);
+  function decrease(){
+setCount(c=>c-1);
+  }
+  return (
+    <div>
+      <button onClick={decrease}>decrease</button>
+    </div>
+  )
+}
+
 
 export default App
---
+++ src/Store/atoms/Counter.js
@@ -0,0 +1,6 @@
+import {atom} from 'recoil'
+
+export const counteratom=atom({
+    key:"counter",
+    default:0
+})
