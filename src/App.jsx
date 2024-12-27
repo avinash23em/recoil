213227fce1878c3305835cf7b3122b94bc3914e0
avinash23em <ap23vi@gmail.com>
2024-12-27 20:38:55 +0530

    intial

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {RecoilRoot, atom , useRecoilValue, useSetRecoilState} from 'recoil'
-import { counteratom } from './Store/atoms/Counter'
+import { counteratom,evenSelector } from './Store/atoms/Counter'
 import { useEffect } from 'react'
 
 
@@ -63,69 +63,113 @@ import { useEffect } from 'react'
 
  ///use memo to correct the issue defined 
 
-function App() {
+// function App() {
   
 
+//   return (
+//     <>
+//       <RecoilRoot>
+//            <Counter/>
+//       </RecoilRoot>
+//     </>
+//   )
+// }
+
+// //here count defined under counter is not passed as props to next components unders it but when after 3secc it renders whole components inside it renders too 
+
+// function Counter(){
+//   const[countt,setCount]=useState(0);
+//   useEffect(()=>{
+//    setInterval(()=>{
+//    setCount(c=>c+1);
+//    },3000)
+//   },[]);
+
+// return (
+  
+//   <div>
+//     <Currcounter/>
+//     <Increase/>
+//     <Decrease/>
+//   </div>
+// )
+// }
+// const Currcounter=memo(function(){
+// const count=useRecoilValue(counteratom);
+// return (
+//   <div>
+//     count {count}
+//   </div>
+// )
+// })
+
+// const Increase=memo (function(){
+//   const setCount=useSetRecoilState(counteratom);
+//   function increase(){
+// setCount(c=>c+1);
+//   }
+//   return (
+//     <div>
+//       <button onClick={increase}>increase</button>
+//     </div>
+//   )
+// })
+
+// const Decrease=memo(function(){
+//   const setCount=useSetRecoilState(counteratom);
+//   function decrease(){
+// setCount(c=>c-1);
+//   }
+//   return (
+//     <div>
+//       <button onClick={decrease}>decrease</button>
+//     </div>
+//   )
+// })
+
+
+
+function App(){
   return (
-    <>
-      <RecoilRoot>
-           <Counter/>
+    <div>
+    <RecoilRoot>
+      <Buttons/>
+      <Counter/>
+      <IsEven/>
       </RecoilRoot>
-    </>
+    </div>
   )
 }
-
-//here count defined under counter is not passed as props to next components unders it but when after 3secc it renders whole components inside it renders too 
-
-function Counter(){
-  const[countt,setCount]=useState(0);
-  useEffect(()=>{
-   setInterval(()=>{
-   setCount(c=>c+1);
-   },3000)
-  },[]);
-
-return (
-  
-  <div>
-    <Currcounter/>
-    <Increase/>
-    <Decrease/>
-  </div>
-)
-}
-const Currcounter=memo(function(){
-const count=useRecoilValue(counteratom);
-return (
-  <div>
-    count {count}
-  </div>
-)
-})
-
-const Increase=memo (function(){
+function Buttons(){
   const setCount=useSetRecoilState(counteratom);
   function increase(){
-setCount(c=>c+1);
+    setCount(c=>c+2);
   }
-  return (
-    <div>
-      <button onClick={increase}>increase</button>
-    </div>
-  )
-})
 
-const Decrease=memo(function(){
-  const setCount=useSetRecoilState(counteratom);
   function decrease(){
-setCount(c=>c-1);
+    setCount(c=>c-1);
+
   }
-  return (
-    <div>
-      <button onClick={decrease}>decrease</button>
-    </div>
-  )
-})
 
 
+  return <div>
+    <button onClick={increase}>increase</button>
+    <button onClick={decrease}>decrease</button>
+  </div>
+}
+
+
+function Counter(){
+  const count=useRecoilValue(counteratom);
+  return <div>
+    {count}
+  </div>
+}
+
+function IsEven(){
+  const even=useRecoilValue(evenSelector);
+return <div>
+  {even ?  "even" :"odd"}
+</div>
+}
 export default App
--- src/Store/atoms/Counter.js
+++ src/Store/atoms/Counter.js
@@ -1,6 +1,14 @@
-import {atom} from 'recoil'
+import {atom, selector} from 'recoil'
 
 export const counteratom=atom({
     key:"counter",
     default:0
+})
+
+export const evenSelector=selector({
+    key:"iseven",
+    get:function({get}){
+        const count=get(counteratom);
+        return count%2==0;
+    }
 })
